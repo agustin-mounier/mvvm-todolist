f1a89a67a6c6cc34ccb90b3cb2dde4aa5663a03a
Agustin Mounier <agusmouni@gmail.com>
2019-07-18 10:50:04 -0300

    separated respository logic into a service

--- app/src/main/java/com/example/viemedtodolist/data/TasksRepository.kt
+++ app/src/main/java/com/example/viemedtodolist/data/TasksRepository.kt
@@ -1,10 +1,8 @@
 package com.example.viemedtodolist.data
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.viemedtodolist.networking.RequestErrorAction
-import com.example.viemedtodolist.networking.VMApolloClient
+import com.example.viemedtodolist.networking.TasksService
 
 /**
  * Tasks repository that acts as a single source of truth for all the app data.
@@ -15,22 +13,34 @@ object TasksRepository {
 
     private var taskList = mutableListOf<Task>()
     private val tasks = MutableLiveData<MutableList<Task>>()
-
-    private var isLoading = MutableLiveData<Boolean>()
-    private var requestErrorAction = MutableLiveData<RequestErrorAction>()
-
-    private val LOG_TAG = "Request exception"
+    private val tasksService = TasksService()
 
     init {
         tasks.value = taskList
-        requestErrorAction.value = RequestErrorAction.NONE
     }
 
-    fun isLoading() = isLoading as LiveData<Boolean>
+    fun isLoading() = tasksService.isLoading()
 
     fun getTasks() = tasks as LiveData<List<Task>> //Cast to prevent modifications
 
-    fun getNetworkError() = requestErrorAction as LiveData<RequestErrorAction>
+    fun getNetworkError() = tasksService.getNetworkError()
+
+    fun createTask(name: String, note: String, isDone: Boolean) {
+        tasksService.createTask(name, note, isDone)
+    }
+
+    fun deleteTask(id: String) {
+        tasksService.deleteTask(id)
+    }
+
+    fun toggleCheck(id: String) {
+        val task = taskList.find { task -> task.id == id }!!
+        tasksService.toggleCheck(id, !task.isDone)
+    }
+
+    fun fetchAllTasks() {
+        tasksService.fetchAllTasks()
+    }
 
     fun addTask(task: Task) {
         taskList.add(task)
@@ -43,57 +53,14 @@ object TasksRepository {
         tasks.postValue(taskList)
     }
 
-    fun setTasks(tasks: List<Task>) {
-        taskList = tasks as MutableList<Task>
-        this.tasks.postValue(taskList)
+    fun setTasks(newTasks: List<Task>) {
+        taskList = newTasks as MutableList<Task>
+        tasks.postValue(taskList)
     }
 
-    fun toggleCheck(id: String) {
+    fun setIsDone(id: String, isDone: Boolean) {
         val task = taskList.find { task -> task.id == id }!!
-        task.isDone = !task.isDone
-        tasks.value = taskList
-        VMApolloClient.updateTaskStatus(id, task.isDone) { _, exception ->
-            exception?.let {
-                Log.d(LOG_TAG, it.toString())
-                requestErrorAction.postValue(RequestErrorAction.UPDATE)
-            }
-        }
-    }
-
-    fun createTask(name: String, note: String, isDone: Boolean) {
-        isLoading.value = true
-        VMApolloClient.createTask(name, note, isDone) { task, exception ->
-            isLoading.postValue(false)
-            if (task != null) {
-                addTask(task)
-            } else {
-                requestErrorAction.postValue(RequestErrorAction.CREATE)
-            }
-
-            exception?.let { Log.d(LOG_TAG, it.toString()) }
-        }
-    }
-
-    fun deleteTask(id: String) {
-        isLoading.value = true
-        VMApolloClient.deleteTask(id) { deleteTask, exception ->
-            if (deleteTask != null && deleteTask) {
-                removeTask(id)
-            } else {
-                requestErrorAction.postValue(RequestErrorAction.DELETE)
-            }
-
-            exception?.let { Log.d(LOG_TAG, it.toString()) }
-        }
-    }
-
-    fun fetchAllTasks() {
-        isLoading.value = true
-        VMApolloClient.getAllTasks { tasks, exception ->
-            if (tasks != null) {
-                setTasks(tasks)
-            }
-            isLoading.postValue(false)
-        }
+        task.isDone = isDone
+        tasks.postValue(taskList)
     }
 }
---
+++ app/src/main/java/com/example/viemedtodolist/networking/TasksService.kt
@@ -0,0 +1,69 @@
+package com.example.viemedtodolist.networking
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.viemedtodolist.data.TasksRepository
+
+class TasksService {
+
+    private val LOG_TAG = "Request exception"
+    private var isLoading = MutableLiveData<Boolean>()
+    private var requestErrorAction = MutableLiveData<RequestErrorAction>()
+
+    init {
+        requestErrorAction.value = RequestErrorAction.NONE
+    }
+
+    fun isLoading() = isLoading as LiveData<Boolean>
+
+    fun getNetworkError() = requestErrorAction as LiveData<RequestErrorAction>
+
+    fun createTask(name: String, note: String, isDone: Boolean) {
+        isLoading.value = true
+        VMApolloClient.createTask(name, note, isDone) { task, exception ->
+            isLoading.postValue(false)
+            if (task != null) {
+                TasksRepository.addTask(task)
+            } else {
+                requestErrorAction.postValue(RequestErrorAction.CREATE)
+            }
+
+            exception?.let { Log.d(LOG_TAG, it.toString()) }
+        }
+    }
+
+    fun deleteTask(id: String) {
+        isLoading.value = true
+        VMApolloClient.deleteTask(id) { deleteTask, exception ->
+            if (deleteTask != null && deleteTask) {
+                TasksRepository.removeTask(id)
+            } else {
+                requestErrorAction.postValue(RequestErrorAction.DELETE)
+            }
+
+            exception?.let { Log.d(LOG_TAG, it.toString()) }
+        }
+    }
+
+    fun fetchAllTasks() {
+        isLoading.value = true
+        VMApolloClient.getAllTasks { tasks, exception ->
+            if (tasks != null) {
+                TasksRepository.setTasks(tasks)
+            }
+            isLoading.postValue(false)
+        }
+    }
+
+    fun toggleCheck(id: String, isDone: Boolean) {
+        VMApolloClient.updateTaskStatus(id, isDone) { isDone, exception ->
+            if (exception == null) {
+                TasksRepository.setIsDone(id, isDone!!)
+            } else {
+                Log.d(LOG_TAG, exception.toString())
+                requestErrorAction.postValue(RequestErrorAction.UPDATE)
+            }
+        }
+    }
+}
